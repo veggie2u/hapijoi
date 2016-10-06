ba032aae3427ac6857a4e91337aef7b27138afa6
Chris Ward <veggie2u@cyberward.net>
2016-10-06 12:28:09 -0500

    create path and query params

--- index.js
+++ index.js
@@ -12,10 +12,22 @@ server.start((err) => {
     console.log(`Server running at: ${server.info.uri}`);
 });
 
+// hello query parameter
 server.route({
     method: 'GET',
     path: '/',
     handler: function (request, reply) {
-        reply('Hello, world!');
+        let name = request.query.name ? encodeURIComponent(request.query.name) : 'world'
+        reply(`Hello, ${name}!`);
     }
 });
+
+// hello path parameter
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
+});
+
