bd5af4e4709b9c0005b7e2581b59fe3d634ab7be
Chris Ward <veggie2u@cyberward.net>
2016-10-06 11:29:12 -0500

    add hello world

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 #Hapi Hapi Joi Joi
 
 This is code for a simple web server using the node based Hapi server, and Joi, a validation library.
+
+Note: the examples use ES6. The project was created on Node v6.2.2
+
+##Install & Run
+`npm install`
+`node index.js`
+
+These commands will pull down the required libraries, and then run the node server defined in index.js.
---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+server.connection({ port: 9000 });
+
+server.start((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log(`Server running at: ${server.info.uri}`);
+});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
