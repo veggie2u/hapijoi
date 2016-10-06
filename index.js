153f4d355cd21cb055d29178efa9cf5f2f9c82eb
Chris Ward <veggie2u@cyberward.net>
2016-10-06 14:32:49 -0500

    paths

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Hapi = require('hapi');
+const Joi = require('joi');
 
 const server = new Hapi.Server();
 server.connection({ port: 9000 });
@@ -22,12 +23,40 @@ server.route({
     }
 });
 
+// hello query parameter with validation
+server.route({
+    method: 'GET',
+    path: '/hello',
+    config: {
+        validate: {
+            query: {
+                name: Joi.string().min(3).max(10).required(),
+                age: Joi.number().integer().min(1).max(50)
+            }
+        }
+    },
+    handler: function (request, reply) {
+        let name = encodeURIComponent(request.query.name);
+        let age = request.query.age ? encodeURIComponent(request.query.age) : null;
+        let yourAge = age ? `Your age of ${age} is valid.` : '';
+        reply(`Hello, ${name}! ${yourAge}`);
+    }
+});
+
 // hello path parameter
 server.route({
     method: 'GET',
-    path: '/{name}',
+    path: '/hello/{name}',
+    config: {
+        validate: {
+            params: {
+                name: Joi.string().min(3).max(10)
+            }
+        }
+    },
     handler: function (request, reply) {
         reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
     }
 });
+
 
